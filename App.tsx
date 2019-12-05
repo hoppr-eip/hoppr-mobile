90e72058397ad6c48b9ea313694f5fab7e2733f5
Marco GAUTIER <marco.gautier@epitech.eu>
2019-12-05 23:55:46 +0100

    feat(contact): add contact form and contact display

--- App.tsx
+++ App.tsx
@@ -12,9 +12,15 @@ import React from 'react';
 import { View } from 'react-native';
 
 import Contact from './components/contact/contact';
+import Header from './components/header';
 class App extends React.Component {
     render() {
-        return <Contact />;
+        return (
+            <View style={{flex: 1}}>
+                <Header/>
+                <Contact/>
+            </View>
+        );
     }
 }
 
--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, StyleSheet } from 'react-native';
 
 import t from 'tcomb-form-native';
 const Form = t.form.Form;
@@ -11,38 +11,64 @@ const contactForm = t.struct({
 
 class Contact extends React.Component {
     private _form;
+    private _defaultMessage: String = "Je suis en danger";
 
     constructor(props: any) {
         super(props);
         this.state = {
             name: '',
-            phone: ''
+            phone: '',
+            message: this._defaultMessage
         };
     }
 
     addContact = () => {
         const value = this._form.getValue();
-        this.setState({
-            name: value.name,
-            phone: value.phone
-        });
-        console.log('value: ', value);
+        if (value && value.name && value.phone) {
+            this.setState({
+                name: value.name,
+                phone: value.phone
+            });
+            console.log('value: ', value);
+        }
     };
 
     render() {
         return (
-            <View>
-                <Form ref={c => (this._form = c)} type={contactForm} />
-                <Button title='Submit' onPress={this.addContact} />
+            <View style={styles.container}>
+                <View style={styles.block}>
+                    <Form ref={c => (this._form = c)} type={contactForm} />
+                    <Button color='#35b37d' title='Submit' onPress={this.addContact} />
+                </View>
                 {this.state.name && this.state.phone ? (
-                    <View>
+                    <View style={[styles.contact, styles.block]}>
                         <Text>Contact Name: {this.state.name}</Text>
                         <Text>Phone Number: {this.state.phone}</Text>
+                        <Text>Message: {this.state.message}</Text>
                     </View>
                 ) : null}
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    block: {
+        paddingTop: 10,
+        padding: 20,
+        borderRadius: 30,
+        backgroundColor: '#F3F3F3'
+    },
+    button: {
+        backgroundColor: '#35b37d'
+    },
+    contact: {
+        marginTop: 20
+    }
+});
 
 export default Contact;
